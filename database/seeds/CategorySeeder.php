5135a1fbb917168bca3d2d978b1548a167e33691
mariagracelinaa <gracezhouu@gmail.com>
2022-02-24 19:10:33 +0700

    category added

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -12,7 +12,47 @@ class CategorySeeder extends Seeder
     public function run()
     {
         DB::table('categories')->insert([
-            'category_name' => 'Alat Kesehatan'
+            'category_name' => 'Analgesik Narkotik'
+        ]);
+
+        DB::table('categories')->insert([
+            'category_name' => 'Analgesik Non Narkotik'
+        ]);
+
+        DB::table('categories')->insert([
+            'category_name' => 'Antipirai'
+        ]);
+
+        DB::table('categories')->insert([
+            'category_name' => 'Nyeri Neuropatik'
+        ]);
+
+        DB::table('categories')->insert([
+            'category_name' => 'Anestetik Lokal'
+        ]);
+
+        DB::table('categories')->insert([
+            'category_name' => 'Anestetik Umum dan Oksigen'
+        ]);
+
+        DB::table('categories')->insert([
+            'category_name' => 'Obat untuk Prosedur Pre Operatif'
+        ]);
+
+        DB::table('categories')->insert([
+            'category_name' => 'Antialergi dan Obat untuk Anafilaksis'
+        ]);
+
+        DB::table('categories')->insert([
+            'category_name' => 'Khusus'
+        ]);
+
+        DB::table('categories')->insert([
+            'category_name' => 'Umum'
+        ]);
+
+        DB::table('categories')->insert([
+            'category_name' => 'Antiepilepsi - Antikonvulsi'
         ]);
     }
 }
