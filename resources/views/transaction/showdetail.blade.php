c33b4dbcbafae7118974f83a8960e94a864350f7
mariagracelinaa <gracezhouu@gmail.com>
2022-04-16 23:13:32 +0700

    week 8 (tampilan tabel di show detail)

--- resources/views/transaction/showdetail.blade.php
+++ resources/views/transaction/showdetail.blade.php
@@ -1,4 +1,17 @@
-@foreach ($medicines as $m)
-    {{$m->generic_name}} {{$m->pivot->price}} {{$m->pivot->quantity}}<br>
+<table class="table table-bordered">
+    <tr>
+        <th>Nama</th>
+        <th>Harga</th>
+        <th>Jumlah</th>
+        <th>Total</th>
+    </tr>
+    @foreach ($medicines as $m)
+        <tr>
+            <td>{{$m->generic_name}}</td>
+            <td>Rp{{$m->pivot->price}}</td>
+            <td>{{$m->pivot->quantity}}</td>
+            <td>{{$m->pivot->quantity * $m->pivot->price}}</td>
+        </tr> 
 
-@endforeach
+    @endforeach     
+</table>
